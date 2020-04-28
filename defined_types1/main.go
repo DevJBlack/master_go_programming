842b7cfc1e04f892baf11a05d9c5834903097316
Devin Black <Devinjamesblack@gmail.com>
2020-04-28 06:51:15 -0600

    defined types in golang

---
+++ defined_types1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type age int        //new type, int is the underlying type
+type oldAge age     //new type, int (not age) is the underlying type
+type veryOldAge age //new type, int (not age) is the underlying type
+
+type km float64
+type mile float64
+
+func main() {
+
+	// new type speed (underlying type uint)
+	type speed uint
+
+	// s1, s2 of type speed
+	var s1 speed = 10
+	var s2 speed = 20
+
+	// performing operations with the new types
+	fmt.Println(s2 - s1) // -> 10
+
+	// uint and speed are different types (they have different names)
+	var x uint
+
+	// x = s1  //error different types
+
+	// correct
+	x = uint(s1)
+	_ = x
+
+	// correct
+	var s3 speed = speed(x)
+	_ = s3
+
+	var pairsToLondon km = 465
+	var distanceInMiles mile
+
+	distanceInMiles = mile(pairsToLondon) / 0.621
+	fmt.Println(distanceInMiles)
+}
